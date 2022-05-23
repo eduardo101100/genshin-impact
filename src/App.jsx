78d66b32dcae0f7348ce2a9c695a78debe9b1ea2
eduardo101100 <eduardolopezurbano@gmail.com>
2022-05-23 17:06:59 -0500

    configurando proecto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+const App = ()  => { 
+
+
+  return (
+
+  <div className="container">
+    <h1> Genshin impact dex</h1>  
+  <hr/>
+  <select>
+    <option value="artifacts">artefactos</option>
+    <option value="boss">jfes</option>
+    <option value="caracteres">personajes</option>
+  </select>
+  </div>
+  );
+};
+
+export default App;
