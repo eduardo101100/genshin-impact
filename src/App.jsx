d8e63cea91aa994f4198763d4c0a10f5a19dbbab
eduardo101100 <eduardolopezurbano@gmail.com>
2022-05-24 17:07:02 -0500

    añadir el api de genshin

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-
+ import { useEffect, useState } from "react";
+import {fetchHelper} from "./helpers/fetchHelper";
 
 const App = ()  => { 
 
+  const tipos = {
+    artifacts:"artefactos",
+    boss:"jefes",
+    characters:"artefactos",
+    consumables:"consumibles",
+    domains:"dominios",
+    elements:"elementos",
+    enemies:"enemigos",
+    materials:"materiales",
+    nations: "naciones",
+    weapons: "armas",
+  };
+
+  const [selects, setSelects] = useState({
+    types: []
+
+  });
+
+const fetchTypes = async () => {
+  const respuesta = await fetchHelper("https://api.genshin.dev/")
+  const {types} = await respuesta.json()
+  console.log({types})
+  setSelects({...selects, types})
+}
+
+
+
+  useEffect(() => {
+    fetchTypes().catch(console.error)
+  
+  }, []);
+  
 
   return (
 
@@ -9,9 +42,14 @@ const App = ()  => {
     <h1> Genshin impact dex</h1>  
   <hr/>
   <select>
-    <option value="artifacts">artefactos</option>
-    <option value="boss">jfes</option>
-    <option value="caracteres">personajes</option>
+    <option value="artifacts">seleccione el tipo de informacion</option>
+   {
+     selects.types.map((type) => (
+<option value={type}> {tipos[type]}</option>
+     
+     )
+     )
+   }
   </select>
   </div>
   );
